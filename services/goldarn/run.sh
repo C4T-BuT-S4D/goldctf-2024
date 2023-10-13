9d2fedb1bf22f8594c991cf42173a851718ee771
b1r1b1r1 <48091103+b1r1b1r1@users.noreply.github.com>
2023-10-13 10:48:57 +0000

    bruh

--- services/goldarn/run.sh
+++ services/goldarn/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-seq 14141 14652 | parallel -j 512 -k /run_gotty.sh
+mkdir -p /home/ctf/.parallel && echo -j 512 -k > /home/ctf/.parallel/run_profile && chmod 400 /home/ctf/.parallel/run_profile
+
+seq 14141 14652 | parallel -J run_profile /run_gotty.sh
