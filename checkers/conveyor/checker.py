4286ac56453c69891b39989758d9a51aa758552d
Artem Mikheev <30644072+renbou@users.noreply.github.com>
2024-02-07 11:06:06 +0300

    dev/conveyor-1: support template and concat generation in checker

--- checkers/conveyor/checker.py
+++ checkers/conveyor/checker.py
@@ -4,11 +4,13 @@ import random
 import sys
 import traceback
 from enum import Enum
+from functools import partial
 from typing import cast
 
+import pandas as pd
 import rpyc
-from checklib import *
-from conveyorlib import GoldConveyorService
+from checklib import BaseChecker, Status, cquit
+from conveyorlib import AlloyComposition, DataConveyor, GoldConveyorService
 
 SERVICE_PORT = 12378
 MAX_SAMPLES = 100
@@ -21,6 +23,13 @@ FEATURES = [
     "troy_ounces",
     "karat",
     "fineness",
+]
+ALLOYS = [
+    AlloyComposition(gold_fr=0.75, silver_fr=0.125, copper_fr=0.125, platinum_fr=0),
+    AlloyComposition(gold_fr=0.75, silver_fr=0, copper_fr=0.25, platinum_fr=0),
+    AlloyComposition(gold_fr=0.75, silver_fr=0.025, copper_fr=0.225, platinum_fr=0),
+    AlloyComposition(gold_fr=0.75, silver_fr=0.05, copper_fr=0.2, platinum_fr=0),
+    AlloyComposition(gold_fr=0.75, silver_fr=0, copper_fr=0, platinum_fr=0.25),
 ]
 
 
@@ -51,15 +60,15 @@ class Checker(BaseChecker):
 
     conn: rpyc.Connection
     service: GoldConveyorService
+    data_conveyor: DataConveyor
 
     def __init__(self, *args, **kwargs):
         super(Checker, self).__init__(*args, **kwargs)
 
     def action(self, action, *args, **kwargs):
         try:
-            self.connect()
             super(Checker, self).action(action, *args, **kwargs)
-            self.disconnect()
+            self._disconnect()
         except ConnectionError as err:
             self.cquit(Status.DOWN, "Connection error", f"Connection error: {err}")
         except TimeoutError as err:
@@ -75,13 +84,8 @@ class Checker(BaseChecker):
                 raise
 
     def check(self):
-        want_weight = rnd_weight()
-        want_deviation = rnd_deviation()
-        want_nsamples = rnd_nsamples()
-
-        df = self.service.data_conveyor.random_alloy_samples(
-            want_weight, want_deviation, want_nsamples
-        )
+        self._connect()
+        (want_weight, want_deviation, want_nsamples), df = self._generate_samples()
 
         self.assert_eq(
             len(df),
@@ -96,21 +100,25 @@ class Checker(BaseChecker):
             Status.MUMBLE,
         )
 
+        self._disconnect()
         self.cquit(Status.OK)
 
     def put(self, _flag_id: str, flag: str, vuln: str):
-        flag_place = self.parse_vuln(vuln)
+        flag_place = self._parse_vuln(vuln)
 
+        self._connect()
+        self._disconnect()
         self.cquit(Status.OK, "public", "private")
 
     def get(self, flag_id: str, flag: str, vuln: str):
-        flag_place = self.parse_vuln(vuln)
-
+        flag_place = self._parse_vuln(vuln)
         assert flag_id == "private"
 
+        self._connect()
+        self._disconnect()
         self.cquit(Status.OK)
 
-    def connect(self):
+    def _connect(self):
         self.conn = cast(
             rpyc.Connection,
             rpyc.connect(
@@ -124,17 +132,41 @@ class Checker(BaseChecker):
         )
 
         self.service = cast(GoldConveyorService, self.conn.root)
+        self.data_conveyor = self.service.data_conveyor
 
-    def disconnect(self):
+    def _disconnect(self):
         self.conn.close()
 
-    def parse_vuln(self, vuln: str) -> FlagPlace:
+    def _parse_vuln(self, vuln: str) -> FlagPlace:  # type: ignore
         if vuln == "1":
             return FlagPlace.DATASET
         elif vuln == "2":
             return FlagPlace.MODEL
         else:
             c.cquit(Status.ERROR, "Checker error", f"Got unexpected vuln value {vuln}")
+
+    def _generate_samples(
+        self,
+    ) -> tuple[tuple[float, float, int], pd.DataFrame]:
+        want_weight = rnd_weight()
+        want_deviation = rnd_deviation()
+        want_nsamples = rnd_nsamples()
+        params = (want_weight, want_deviation, want_nsamples)
+
+        generators = [
+            self.data_conveyor.random_alloy_samples,
+            partial(self.data_conveyor.template_alloy_samples, random.choice(ALLOYS)),
+        ]
+
+        generators.append(
+            lambda w, d, n: self.data_conveyor.concat_samples(
+                generators[0](w, d, n // 2), generators[1](w, d, n - n // 2)
+            )
+        )
+
+        return params, random.choice(generators)(
+            want_weight, want_deviation, want_nsamples
+        )
 
 
 if __name__ == "__main__":
--- checkers/conveyor/conveyorlib.py
+++ checkers/conveyor/conveyorlib.py
@@ -2,16 +2,42 @@
 conveyorlib contains rpyc service type stubs for interacting with the service
 """
 
+from dataclasses import dataclass
 from typing import Optional
 from uuid import UUID
 
 import pandas as pd
 
 
+@dataclass
+class AlloyComposition:
+    ALLOWED = set(["gold_fr", "silver_fr", "copper_fr", "platinum_fr"])
+
+    gold_fr: float
+    silver_fr: float
+    copper_fr: float
+    platinum_fr: float
+
+    def _rpyc_getattr(self, name):
+        if name in AlloyComposition.ALLOWED:
+            return getattr(self, name)
+        raise AttributeError("access denied")
+
+
 class DataConveyor:
     def random_alloy_samples(
         self, weight_ozt: float, max_deviation: float, samples: int
     ) -> pd.DataFrame: ...
+
+    def template_alloy_samples(
+        self,
+        template: AlloyComposition,
+        weight_ozt: float,
+        max_deviation: float,
+        samples: int,
+    ) -> pd.DataFrame: ...
+
+    def concat_samples(self, *dfs: pd.DataFrame) -> pd.DataFrame: ...
 
 
 class ModelConveyor:
