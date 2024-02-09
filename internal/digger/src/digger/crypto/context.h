ffef5cb637f9a318966eb97ae15b1fc9d6128813
Vladimir <keltecc@ya.ru>
2024-02-09 05:55:28 +0300

    [digger] fix context

--- internal/digger/src/digger/crypto/context.h
+++ internal/digger/src/digger/crypto/context.h
@@ -21,16 +21,16 @@ namespace Digger::Crypto {
         );
 
     public:
-        Table<56> PC1;
-        Table<48> PC2;
-        Table<16> KeyRotation;
+        Table<56> PC1  = Table<56>();
+        Table<48> PC2  = Table<48>();
+        Table<16> KeyRotation = Table<16>();
 
-        Table<64> InitialPermutation;
-        Table<32> RoundPermutation;
-        Table<64> FinalPermutation;
+        Table<64> InitialPermutation = Table<64>();
+        Table<32> RoundPermutation = Table<32>();
+        Table<64> FinalPermutation = Table<64>();
 
-        Table<48> Expansion;
-        Mapping<64> Sbox;
+        Table<48> Expansion = Table<48>();
+        Mapping<64> Sbox = Mapping<64>();
     };
 
 }; /* namespace Digger::Crypto */
